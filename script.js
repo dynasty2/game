75bcb5b1661faf07f68dd84da2eada1e1f60280c
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-10-22 11:09:35 -0400

    clickable start and submit and bigger font

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const box2 = document.querySelector("#box2")
 const box3 = document.querySelector("#box3")
 const box4 = document.querySelector("#box4")
 const box5 = document.querySelector("#box5")
+const start = document.querySelector("#box7")
+const submit =document.querySelector("#box8")
 
 box2.addEventListener("click", function(evt) {
 		evt.preventDefault();
@@ -25,6 +27,18 @@ box5.addEventListener("click", function(evt) {
 		evt.preventDefault();
 		console.log(evt);
 		console.log("You clicked box 5.")
+})
+
+box7.addEventListener("click", function(evt) {
+		evt.preventDefault();
+		console.log(evt);
+		console.log("You clicked Start.")
+})
+
+box8.addEventListener("click", function(evt) {
+		evt.preventDefault();
+		console.log(evt);
+		console.log("You clicked box Submit.")
 })
 
 const questions = [
