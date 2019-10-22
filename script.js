1629adfe0cc9e3294348679d89e9c76efe373d6b
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-10-22 13:17:22 -0400

    five questions and answers

--- script.js
+++ script.js
@@ -33,7 +33,11 @@ box7.addEventListener("click", function(evt) {
 		evt.preventDefault();
 		console.log(evt);
 		console.log("You clicked Start.")
-		startRound()
+		// startRound()
+		// startRound2()
+		// startRound3()
+		// startRound4()
+		startRound5()
 })
 
 box8.addEventListener("click", function(evt) {
@@ -42,17 +46,47 @@ box8.addEventListener("click", function(evt) {
 		console.log("You clicked box Submit.")
 })
 
-const questions = [
-["Which professional National Xball League team won the 2015 World Cup?"]
-]
+const questions = ["Which professional National Xball League team won the 2015 World Cup?", 
+"Which team did Ollie Lang begin his professional career with?", 
+"What teamn was on the cover of the 2009 PlayStation 2 NPPL video game?", 
+"In what year did San Diego Dynasty and Edmonton Impact combine their rosters to create the team known as Dynapact?",
+"What is the most sold paintball marker of all time?"];
+
+
 
 const answers = [
-["San Diego Dynasty", "Houston Heat", "Moscow Red Legion", "San Antonio X-Factor"]
-]
+["San Diego Dynasty", "Houston Heat", "Moscow Red Legion", "San Antonio X-Factor"],
+["Los Angeles IronMen", "San Diego Dynasty", "Philly All-Americans", "Tampa-Bay Damage"],
+["San Diego Dynasty", "Sacremento XSV", "Seattle Thunder", "Baltimore Revo"],
+["2010", "2013", "2019", "2005"],
+["Tippmann 98-Custom", "Planet Eclipse Etek5", "Spyder Fenix", "Empire Axe"]
+
 
 const questionContent = document.querySelector(".question")
+const round = 0;
 
-function startRound() {
-	for (let i = 0; i < questions.length; i++)
-		questionContent.innerText = questions[i]
+function startRound() { 
+	questionContent.innerText = questions[0]		
 }
+		
+function startRound2() {
+	questionContent.innerText = questions[1]
+}
+
+function startRound3() {
+	questionContent.innerText = questions[2]
+}
+
+function startRound4() {
+	questionContent.innerText = questions[3]
+}
+
+function startRound5() {
+	questionContent.innerText = questions[4]
+}
+
+
+
+
+
+
